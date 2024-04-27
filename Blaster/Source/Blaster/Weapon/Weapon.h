70a2652746cd6cb06689205ed15e14a84b80533f
김민준 <79951019+kmj5515@users.noreply.github.com>
2024-04-27 12:31:42 +0900

    CombatComponent 활용하여 Weapon 장착

--- Blaster/Source/Blaster/Weapon/Weapon.h
+++ Blaster/Source/Blaster/Weapon/Weapon.h
@@ -59,5 +59,5 @@ private:
 	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
 	class UWidgetComponent* PickupWidget;
 public:	
-
+	FORCEINLINE void SetWeaponState(EWeaponState State) { WeaponState = State; }
 };
