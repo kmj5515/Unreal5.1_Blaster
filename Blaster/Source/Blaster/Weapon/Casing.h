52ef6c4415f0cc2b01a1bf06c1331b359665cf75
김민준 <79951019+kmj5515@users.noreply.github.com>
2024-05-12 21:27:16 +0900

    Bullet shells

---
+++ Blaster/Source/Blaster/Weapon/Casing.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Casing.generated.h"
+
+UCLASS()
+class BLASTER_API ACasing : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ACasing();
+private:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* CasingMesh;
+
+protected:
+	virtual void BeginPlay() override;
+};
